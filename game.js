c123b4d4b6d4dfe4ba1bfe89e5f9668294d4545e
jonapy <jonacanovale@gmail.com>
2022-08-18 22:12:28 -0600

    made game loop work with oop, jump up, and draw funcs

--- game.js
+++ game.js
@@ -3,40 +3,80 @@
 
 /* --------------------------------------------------------- */
 /* Function Oriented Programming */
-
-function gameRun(mapMatrixObject, marioObject) {
-
-	requestAnimationFrame(gameRun);						// Update the game very fast by calling the gameRun function over and over again
-} 
-
 function main() {
-
-	canvas = document.getElementById("canvas");				// Reference to the canvas in the page
-	ctx = canvas.getContext("2d");							// 2d context used for several useful methods
+	let canvas = document.getElementById("canvas");			// Reference to the canvas in the page
+	let context = canvas.getContext("2d");					// 2d context used for several useful methods
 	let mapMatrix = new MatrixMap(50, 150);					// Map Object
-	let mario = new Mario(4, 0);							// Mario Object
+	let mario = new Mario(context);							// Mario Object
+	let background = new Background(canvas, context, mario);// Moving Background object
+	const gravity = 1.2;
+	const jumptAmount = 20;
+	//Game Loop
+	let gameRun = function () {
 
+		// Update the game very fast by calling the gameRun function over and over again
+		context.clearRect( 0, 0, background.getWidth(), background.getHeight() );
+		background.draw();
+		mario.draw();
+		console.log(mario.getyPos());
+		console.log(mario.getxPos());
+		
+		// falling condition
+		if ( mario.getyPos() + mario.getFallingVel() + mario.getHeight() <= background.getHeight() ){
+			mario.setyPos( mario.getyPos() + mario.setFallingVel( mario.getFallingVel() + gravity ) );
+		}	
+		else 
+			mario.setFallingVel(0);
+		
+		if ( mario.getRunningVel >= 2 )
+			mario.setRunningVel(2);
+		console.log("looping the game");
+		
+		requestAnimationFrame(gameRun);	
+	} 
+
+	// ---- Add all event listeners before running the game loop ---- //
 	document.addEventListener('keydown', (event) => {
-
-		if (event.key == "w")
-			mapMatrix.updateMatrixUp(mario);
-		else if (event.key == "a") 
-			mapMatrix.updateMatrixLeft(mario);
-		else if (event.key == "d") 
-			mapMatrix.updateMatrixRight(mario);
+		if (event.key == "w"){
+		    mario.setFallingVel( mario.getFallingVel() - jumptAmount);
+			//mapMatrix.updateMatrixUp(mario);
+		}
+		else if (event.key == "a"){
+			// check left bound of the screen (could be a function later)
+			if ( mario.getxPos() - mario.getWidth() >= 0 ) {
+				mario.setxPos( mario.getxPos() - mario.setRunningVel( mario.getRunningVel() + mario.getAcceleration() ) ); // problem here!! returnin NAN
+				//platformDirection = "negative"; keep track of running direction!!
+			}
+			//mapMatrix.updateMatrixLeft(mario);
+		}
+		else if (event.key == "d"){
+			// check right bound of the screen
+			if ( mario.getxPos() + mario.getWidth() + mario.getAcceleration() <= background.getWidth() ) {
+				mario.setxPos( mario.getxPos() + mario.setRunningVel( mario.getRunningVel() + mario.getAcceleration() ) );
+				//platformDirection = "positive"; here too !!
+			}
+			//mapMatrix.updateMatrixRight(mario);
+		}
 	});
 
-	document.addEventListener('keyup', (event) => {		// we need the keyup to map the falling
+	document.addEventListener('keyup', (event) => {
 
-		if (event.key == "w")
+		if (event.key == "w"){
+			mario.setFallingVel( mario.getFallingVel() + jumptAmount);
 			mapMatrix.updateMatrixUp(mario);
+		}	
 		else if (event.key == "a") 
 			mapMatrix.updateMatrixLeft(mario);
 		else if (event.key == "d") 
 			mapMatrix.updateMatrixRight(mario);
+		
+		mario.setRunningVel(0);
 	});
 	
-	//gameRun(mapMatrix, mario);
+	// Should we list all parameters in here?
+	// we could pass all objects to update into an abstract object list and hand them to gameRun!!!
+	//mario.draw();
+	requestAnimationFrame( gameRun );
 }
 
 /* --------------------------------------------------------- */
@@ -48,7 +88,7 @@ class MatrixMap {
 	
 	this.mapX = mapX;
 	this.mapY = mapY;
-	this.map = createMap();
+	this.map = this.createMap();
   }
 
   getMatrixMap() {return this.map;}			// Accessor Method to return matrix ( pass a handle to the Mario Class )
@@ -72,82 +112,95 @@ class MatrixMap {
 	// for loop the amount of columns and add at row 0 + 1 more in the y direction
 	// Sleep
 	// Another row etc...
+	return 0;
   }
 
   updateMatrixLeft(objectMove) {
-
+	/*
 	mapMatrix[objectMove.getxPos][objectMove.getyPos] = 0;
 	mapMatrix[objectMove.getxPos][objectMove.getyPos + objectMove.getWidth] = 0;
 	mapMatrix[objectMove.getxPos - objectMove.getWidth][objectMove.getyPos] = objectMove.getMatrixValue;
 	mapMatrix[objectMove.getxPos - objectMove.getWidth][objectMove.getyPos - objectMove.getWidth] = objectMove.getMatrixValue;
+	*/
   }
 
   updateMatrixRight(objectMove) {
-
+	/*
 	mapMatrix[objectMove.getxPos][objectMove.getyPos] = 0;
 	mapMatrix[objectMove.getxPos][ objectMove.getyPos + objectMove.getWidth] = 0;
 	mapMatrix[objectMove.getxPos + objectMove.getWidth][objectMove.getyPos] = objectMove.getMatrixValue;
 	mapMatrix[objectMove.getxPos + objectMove.getWidth][objectMove.getyPos + objectMove.getHeight] = objectMove.getMatrixValue;
-  }	
+	*/
+}	
 }
 
 class Mario {
-	constructor() {
-				
-		this.width = 2;									// Size in blocks ( not pixels )
-		this.height = 4;
-
-		this.xPos = 0;					     			// The Mario character will have a width of 1 block and a height of 2 blocks in the matrix representation
-		this.yPos = this.height;
+	constructor(context) {
+		this.context = context;		
+		this.width = 50;									// Size in blocks ( not pixels )
+		this.height = 50;
+		this.xPos = 100;					     			// The Mario character will have a width of 1 block and a height of 2 blocks in the matrix representation
+		this.yPos = 100;
+		let fallingVelocity = 2;
+		let runningVelocity = 0.2;
+		let acceleration = 0.2;
 	}
 
 	getxPos() {return this.xPos;}
 	getyPos() {return this.yPos;}
-
 	getWidth() {return this.width;}
 	getHeight() {return this.height;}
-
 	getMatrixValue() {return 1;}
-}
+	getRunningVel() {return this.runningVelocity;}
+	getFallingVel() {return this.fallingVelocity;}
+	getAcceleration() {return this.acceleration;}
 
-class entityGraphic() {
-	constructor() {
-		// should be a generic position given by the user
-		let xPos = 20;
-		let yPos = 500;
-		const height = 50;
-		const width = 50;
-		let fallingVelocity = 2;
-		let runningVelocity = 0.2;
-		let acceleration = 0.2;
-		const gravity = 1.2;
-		//draw the object
-		ctx.fillRect( xPos, yPos, height, width );
-		ctx.fillStyle = "red";	
+	setxPos( num ) {
+		this.xPos = num;
+		return this.xPos;
+	}
+	setyPos( num ) {
+		this.yPos = num;
+		return this.yPos;
+	}
+	setFallingVel( num ) { 
+		this.fallingVelocity = num;
+		return this.fallingVelocity;
+	}
+	setRunningVel( num ) { 
+		this.runningVelocity = num;
+		return this.runningVelocity;
 	}
 
-
+	// Draws Mario in its current position on the screen
+	draw() {
+		this.context.fillRect( this.xPos, this.yPos, this.height, this.width );
+		this.context.fillStyle = "red";	
+		return 1;
+	}
+}
 
 class Background {
-	constructor() {
+	constructor(canvas, context, mario) {
 		this.canvas = canvas;
-		this.ctx = ctx;
+		this.ctx = context;
+		this.mario = mario;
+		this.height = 672;
+		this.width = 10176;
 		this.image = document.getElementById('background');
-		this.drawBackground();
 	}
 
-	// Move as mario advances
-	drawBackground() {
-		this.ctx.drawImage(this.image, 7, 7, 10176, 672);
-	}
+	getHeight() { return this.height; }
+	getWidth() { return this.width; }
 
+	// Code the background Movement as mario advances !!
+	draw() { 
+		this.ctx.drawImage(this.image, 7, 7, this.width, this.height); 
+		return 1;
+	}
 }
 
-new Background();
-
-
-
-
+main();
 
 /*
 let xPos = 20;
@@ -161,18 +214,6 @@ const gravity = 1.2;
 
 let platformX = 200;
 
-let canvas = document.getElementById( "wallpaper" );			
-let context = canvas.getContext( "2d" );
-
-context.fillRect( xPos, yPos, 50, 50 );
-context.fillStyle = "red";	
-
-const characterHeight = 50;
-const characterWidth = 50;
-
-const keys = { right: { pressed: false },
-	        left: { pressed: false } 
-	     }
 
 function gameRun( ) {
 	
